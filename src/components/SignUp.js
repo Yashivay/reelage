02861487885b87108a6e2706d4c605ff11bc3de7
Yashivay <111106524+YashKumar95@users.noreply.github.com>
2022-10-13 18:13:34 +0530

    new code

---
+++ src/components/SignUp.js
@@ -0,0 +1,65 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import logo from './signUp.png';
+
+
+function App() {
+  return (
+    <div class="MainBody">
+      <div className="container"> 
+        <div className="SignBody"> 
+
+          <div className="row">
+            <div className="col-6">
+              <div className="SignFormBox">  
+                <div className='SignFormBoxHeader'> 
+                  <h1> Sign up </h1>
+                  <p>Please enter your detail</p>
+                </div>
+
+
+                <Form>
+
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                  </Form.Group>
+
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                  
+                </Form>
+
+              </div>
+            </div> 
+            <div className="col-6">          
+                {/* <img src="signUp.png"/> */}
+                <div class="signUpImg"> 
+                  <img src={logo} className="App-logo" alt="logo" />  
+                </div>            
+              </div> 
+          </div>
+        </div>
+      </div>     
+    </div>    
+  );
+}
+
+export default App;
